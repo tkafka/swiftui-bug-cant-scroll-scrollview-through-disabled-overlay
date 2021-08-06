732c3142c35ea2a788c9462cf2d076e9bde53ee0
Tomáš Kafka <tk@tomaskafka.com>
2021-08-06 18:00:01 +0200

    Commit of bug

---
+++ README.md
@@ -0,0 +1,11 @@
+# Expected behavior
+
+Element with `.allowsHitTesting(false).disabled(true)` should not swallow any user interactions.
+
+# Actual behavior
+
+Clicks at buttons might happen through the element, but all gestures are swallowed - user cannot scroll the underlying ScrollViews if the origin of touch begins in a red overlay rectangle.
+
+# Video:
+
+![Image of problem](swiftui-scrollview-fail.gif)
--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -7,11 +7,97 @@
 
 import SwiftUI
 
+struct HorizontalView: View {
+	var body: some View {
+		ScrollView(.horizontal, showsIndicators: false) {
+			HStack {
+				Group {
+					Text("Scroll this ->")
+					Text("B")
+					Text("C")
+					Text("D")
+					Text("E")
+					Text("F")
+					Text("G")
+					Text("H")
+					Text("I")
+					Text("J")
+				}
+				Group {
+					Text("K")
+					Text("L")
+					Text("M")
+					Text("N")
+					Text("O")
+					Text("P")
+					Text("Q")
+					Text("R")
+					Text("S")
+					Text("T")
+				}
+			}
+		}
+	}
+}
+
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
+		var body: some View {
+			ZStack(alignment: Alignment(horizontal: .trailing, vertical: .top)) {
+				
+				/// overlay that stays on top during scroll
+				/// this should not block the drag gestures for the underlying ScrollViews, but it absolutely does!
+				Rectangle()
+					.background(Color.red)
+					.frame(width: 128, height: 192)
+					.zIndex(1)
+					.opacity(0.25)
+					.allowsHitTesting(false)
+					.disabled(true)
+				
+				ScrollView(.vertical) {
+					VStack {
+						HorizontalView()
+						
+						Text("You can tap the button through the red view, but you cannot scroll the scrollviews, even though the Rectangle is disabled and should have no hit testing!")
+						
+						Button(action: {}) {
+							Text("Tap me")
+						}
+							.frame(idealWidth: .infinity, maxWidth: .infinity)
+						Text("Line 1")
+				
+						Group {
+							Text("Line 2")
+							Text("Line 3")
+							Text("Line 4")
+							Text("Line 5")
+							Text("Line 6")
+							Text("Line 7")
+							Text("Line 8")
+							Text("Line 9")
+							Text("Line 10")
+							Text("Line 11")
+						}
+						Group {
+							Text("Line 12")
+							Text("Line 13")
+							Text("Line 14")
+							Text("Line 15")
+							Text("Line 16")
+							Text("Line 17")
+							Text("Line 18")
+							Text("Line 19")
+							Text("Line 20")
+							Text("Line 21")
+						}
+					}
+				}
+				.zIndex(0)
+
+			} /// zstack
+			.frame(width: 256, height: 256)
+			.background(Color.white)
+		}
 }
 
 struct ContentView_Previews: PreviewProvider {
