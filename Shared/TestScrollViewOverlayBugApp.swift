02ac15770fc7efa444c96ee9f064268b4c85c9af
Tomáš Kafka <tk@tomaskafka.com>
2021-08-06 17:37:58 +0200

    Initial Commit

---
+++ Shared/TestScrollViewOverlayBugApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestScrollViewOverlayBugApp.swift
+//  Shared
+//
+//  Created by Tomas Kafka on 06.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct TestScrollViewOverlayBugApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
