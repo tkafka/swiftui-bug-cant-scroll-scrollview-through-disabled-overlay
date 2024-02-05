800ca4ad331c80f8b493ba041ad7735e8f803c4b
Tomáš Kafka <tk@tomaskafka.com>
2024-02-05 09:55:49 +0100

    Add a watch target

---
+++ testoverlaywatch Watch App/testoverlaywatchApp.swift
@@ -0,0 +1,15 @@
+//
+// Created by Tomas Kafka on 05.02.2024.
+// Copyright © 2024 ___ORGANIZATIONNAME___. All rights reserved.
+// 
+
+import SwiftUI
+
+@main
+struct testoverlaywatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
